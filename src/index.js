f52c7e2f64cfcab4f1c34b6afa64ecdc062936e4
Алексей Бельченков <u608110@gmail.com>
2020-01-24 17:49:02 +0400

    Rendering a React project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('movie list');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>movieList</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
