2d60ba057dd67a87ddcaf61b08c3f94dc0d61773
Алексей Бельченков <u608110@gmail.com>
2020-01-24 18:51:59 +0400

    Retrieving data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
-class Card extends Component {
-    render() {
-        return <h1>movieList</h1>
-    }
-}
+const Card = ({ movie }) => {
+    return (
+        <div>
+            <h2>{`#${movie.ranking} - ${movie.title}(${movie.year})`}</h2>
+            <img src={movie.img.src} alt={movie.img.alt} width='200' />
+            <p>{`Distributor: ${movie.distributor}`}</p>
+            <p>{`Amount: ${movie.amount}`}</p>
+        </div>
+    );
+};
 
 export default Card;
